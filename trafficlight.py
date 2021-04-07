156b8c971dbf100731d9aa92336dd5eb98d9a535
ujjwol75 <ujjwolneupane75@gmail.com>
2021-04-07 20:06:27 +0545

    traffic commit

--- trafficlight.py
+++ trafficlight.py
@@ -8,35 +8,42 @@ rect_width = 40
 rect_height = 150
 color = (0, 0, 0)
 bus_x = 690
-bus_y = 212
+bus_y = 211
 bus_vel_x = -3
-# zebracross line variables
+#vertical line variables
 zebraline_x = 150
 zebraline_y = 200
-# road
+#road 
 road_x = 0
 road_y = 225
-#traffic stand variables
-stand_width = 15
-stand_height = 80
+#stand variables
+stand_width = 10
+stand_height = 68
 
 FPS = 80
-# color variables
-red = (255, 0, 0)
-green = (0, 255, 0)
-yellow = (255, 255, 0)
+#color variables
+red = (255,0,0)
+green = (0,255,0)
+yellow = (255,255,0)
+#horizontal line variables(for road)
+first_hr_lineX1 = 0
+first_hr_lineY1 = 235
+first_hr_lineX2 = 800
+first_hr_lineY2 = 235
+
+second_hr_lineX1 = 0
+second_hr_lineY1 = 290
+second_hr_lineX2 = 800
+second_hr_lineY2 = 290
+
+def trafficLightStand():
+    light_stand = pygame.draw.rect(screen, (0,191,255), (26, 170, stand_width, stand_height))
 
 
 def draw_road():
     roadimg = pygame.image.load("road.jpg")
     roadimg = pygame.transform.scale(roadimg, (800, 70))
     screen.blit(roadimg, (road_x, road_y))
-
-
-def trafficlight_stand(color):
-    light_stand = pygame.draw.rect(
-        screen, color, (100,100, stand_width, stand_height))
-
 
 # draw a traffic light rectangle
 def rectangle(color):
@@ -89,14 +96,13 @@ game = True
 running = True
 while game == True:
     fpsClock = pygame.time.Clock()
-
+    
     bus_x = bus_x + bus_vel_x
 
     screen.fill((0, 0, 0))
 
     rectangle((255, 255, 255))
-    
-    trafficlight_stand((255, 255, 255))
+    trafficLightStand()
     draw_first_circle(color)
     draw_second_circle(color)
     draw_third_circle(color)
@@ -128,6 +134,14 @@ while game == True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
+            
+                
 
+     # check if bus is near to line
+    # check_bus()
+
+    
+    # draw_hrLines()
+   
     pygame.display.update()
     fpsClock.tick(FPS)
